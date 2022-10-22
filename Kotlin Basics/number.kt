9811c33616bcae5fe36fad28418f9b4935b00816
srajan-kiyotaka <srajanstark.ash@gmail.com>
2022-10-23 03:33:58 +0530

    Learn Number DataType

---
+++ Kotlin Basics/number.kt
@@ -0,0 +1,13 @@
+fun main() {
+    var x : Int = 15
+    var y : Int = 7
+    var z : Int = 5
+    var result : Int = x + y + z
+    println(result)
+    println(x + y)
+    println(x / y)
+    println(x * z)
+    println("result x + z = ${x + z}")
+    x += 10
+    println("result x + z = ${x + z}")
+}
