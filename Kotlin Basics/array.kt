3334d20d2b73bbaa8b915a0991f3165d5db500a9
srajan-kiyotaka <srajanstark.ash@gmail.com>
2022-10-23 03:36:45 +0530

    Learn Array

---
+++ Kotlin Basics/array.kt
@@ -0,0 +1,16 @@
+fun main() {
+    var vector = arrayOf(2, 6, 9, 10, 45, 87)
+    var stringVector = arrayOf("srajan", "chourasia", "yoo")
+    var mixVector = arrayOf(23, 67, 34, "somesh", "honey")
+    println(vector)
+    println(stringVector)
+    println(mixVector)
+
+    // print an element
+    println("vector: 0th: ${vector[0]}")
+    println(stringVector[0])
+    println(mixVector[0])
+    println(vector[1])
+    println(stringVector[1])
+    println(mixVector[4])
+}
