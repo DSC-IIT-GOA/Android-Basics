7f2f8e7b1cc248cdb559c9df9586fd726628b94b
srajan-kiyotaka <srajanstark.ash@gmail.com>
2022-10-23 03:34:23 +0530

    Learn String DataType

---
+++ Kotlin Basics/string.kt
@@ -0,0 +1,16 @@
+fun main() {
+    var name : String = "Srajan Chourasia"
+    var length : Int = name.length
+    var uppercase : String = name.uppercase()
+    var lowercase : String = name.lowercase()
+    var reversr : String = name.reversed()
+    
+    println("THE STRING IS : $name")
+    println("THE length of the string IS : ${length}")
+    println("THE uppercase of the string IS : $uppercase")
+    println("THE lowercase of the string IS : $lowercase")
+
+    // Home Work
+    println("THE reverse of the string IS : $reversr")
+    // println("THE length of the string IS : ${length}")
+}
